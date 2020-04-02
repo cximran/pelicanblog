ed65c76919cfc68fac226365511094301219a034
Imran Malek <cximran@users.noreply.github.com>
2020-04-01 21:19:22 -0400

    trying a theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -30,6 +30,6 @@ SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = False
-
+THEME = "pelican-themes/dev-random3"
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
